434748010587017a6056c35c09afb56f6643be8c
julio cesar calderon galindo <julcescalgalty@gmail.com>
2022-02-18 01:36:58 -0500

    preuba2

--- helpers/index.js
+++ helpers/index.js
@@ -1,12 +1,10 @@
 const dbValidators = require("./db-validators");
 const generarJWT = require("./generar-jwt");
 const subirArchivo = require("./subir-archivo");
-const notificacion = require("./notificacion");
 const fcValidators =require('./fc-validators');
 module.exports = {
   ...dbValidators,
   ...generarJWT,
   ...subirArchivo,
-  ...notificacion,
   ...fcValidators
 };
